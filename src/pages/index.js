fac5b6202ee679c191d2cbf7d288632c4ef501a7
timschuetz <38264390+timschuetz@users.noreply.github.com>
2020-08-08 14:19:43 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,14 @@ const IndexPage = () => (
     <h1>About</h1>
     <p>This project provides a growing overview of datasets, maps and visualizations that document facilities with potential for "Fast Disaster" in Southern California.</p>
     
-    <p>Get in touch with Tim Sch&uuml;tz (<a href="mailto:tschuetz@uci.edu">tschuetz@uci.edu</a>) if you would like to contribute to any of the mapping projects.</p>
-    
     <p>The website serves as a companion to the course <a href="https://disaster-sts-network.org/content/environmental-injustice-summer-2020/essay">Environmental Injustice</a>, taught by Prof. Kim Fortun at the University of California Irvine.</p>
     
+    <p>Get in touch with Tim Sch&uuml;tz (<a href="mailto:tschuetz@uci.edu">tschuetz@uci.edu</a>) if you would like to contribute to any of the mapping projects.</p>
+    
     <h2>Ammonium Nitrate (Fertilizer) Storage</h2>
-    <p>We are currently working on a map visualizing the storage of ammonium nitrate in California. Get in touch with Tim Sch&uuml;tz (<a href="mailto:tschuetz@uci.edu">tschuetz@uci.edu</a>) if you would like to contribute to the project.</p>
+    <p>We are currently working on a map visualizing the storage of ammonium nitrate in California.</p>
                                                                                                                          
     <p><iframe src="https://timschuetz.github.io/ammonium-nitrate-facilities-ca/" width="100%" height="400px"></iframe></p>                                                                                                                    
-    
     <h2>Risk Management Plan (RMP) Facilities</h2>
     <p>Visualized with <a href="https://rtk.rjifuture.org/rmp/">data</a> by the Right-to-Know-Network and Houston Chronicle. Created and maintained by Tim Sch&uuml;tz, Rieko Konishi and Timothy Do.</p>
         
@@ -27,8 +26,6 @@ const IndexPage = () => (
    
     <p><iframe src="https://timschuetz.github.io/RMP-Facilities-Los-Angeles-County/" width="100%" height="400px"></iframe></p>
         
-    
-       
 
   </Layout>
 )
