ce0c10db443de68c1dc1951c07998780863ddaf7
timschuetz <38264390+timschuetz@users.noreply.github.com>
2020-08-08 08:44:31 -0700

    color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#2EFB60`,
-        theme_color: `#2EFB60`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
