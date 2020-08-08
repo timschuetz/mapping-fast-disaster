1fa5cc77f4030901a015ce065fffa1fe9670fa02
timschuetz <38264390+timschuetz@users.noreply.github.com>
2020-08-08 11:54:39 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,13 +21,13 @@ const IndexPage = () => (
     <p><iframe src="https://timschuetz.github.io/ammonium-nitrate-facilities-ca/" width="100%" height="400px"></iframe></p>                                                                                                                    
     
     <h2>Risk Management Plan (RMP) Facilities</h2>
-        
-    <h3>Los Angeles County</h3>
-    <p><iframe src="https://timschuetz.github.io/RMP-Facilities-Los-Angeles-County/" width="100%" height="400px"></iframe></p>
-        
     <p>Visualized with <a href="https://rtk.rjifuture.org/rmp/">data</a> by the Right-to-Know-Network and Houston Chronicle. Created and maintained by Tim Sch&uuml;tz, Rieko Konishi and Timothy Do.</p>
         
     <p>To contribute to the map comment on the <a href="https://docs.google.com/spreadsheets/d/104wMMATFPWQAmEt6iCvCow0qHG0NiMeZUqC4rBlxrso/edit#gid=0">data table</a> hosted on Google Sheets. You can also make pull requests on <a href="https://github.com/timschuetz/RMP-Facilities-Los-Angeles-County">GitHub</a>.</p>
+   
+    <p><iframe src="https://timschuetz.github.io/RMP-Facilities-Los-Angeles-County/" width="100%" height="400px"></iframe></p>
+        
+    
        
 
   </Layout>
